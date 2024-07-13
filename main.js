93c1212f85f14f3417b6b0579f10ddc858e174c3
VoidOfMaya <mayaofthevoid@gmail.com>
2024-07-13 19:54:26 +0300

    intialize basic elements

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const myLibrary =[];
 
-function Book(){
-    //constuctor
+function Book(author, title, pageNo, read, publishDate){
+    //constuctor (author/title/number of pages/read status/publish date)
+    this.author = author;
+    this.title = title;
+    this.pageNo = pageNo;
+    this.read = read;
+    this.publishDate = publishDate;
 }
 function addBookToLibrary(){
     //do stuff here
 }
+function displayBooks (array){
+    for(let i = 0; i < array.length; i++){
+        console.log(array[i]);
+    }
+}
+let bookOne = new Book("Ekhart Tole","The Power Of Now", 300, false, 2001,);
+
+let bookTwo = new Book("Frank Herbert", "Dune", 530, true, 1987,);
+
+let bookThree = new Book("Danni Davido", "Magnum Dong", 2400, false, 2010,); 
+
+myLibrary.push(bookOne);
+myLibrary.push(bookTwo);
+myLibrary.push(bookThree);
+displayBooks(myLibrary);
