5174f4184c00d99aca8c40a66c890a90286d836d
VoidOfMaya <mayaofthevoid@gmail.com>
2024-07-14 19:50:50 +0300

    adds basic DOM functionality,displays each book item info

--- main.js
+++ main.js
@@ -13,16 +13,65 @@ function addBookToLibrary(){
 }
 function displayBooks (array){
     for(let i = 0; i < array.length; i++){
-        console.log(array[i]);
+        let cardString= Array.from(Object.values(array[i]));
+        console.log(cardString);
+        bookCard(cardString);
     }
 }
-let bookOne = new Book("Ekhart Tole","The Power Of Now", 300, false, 2001,);
+function bookCard(bookArr){
+    //spliting array to indevidual objects  
+    const author = bookArr[0];
+    const title = bookArr[1];
+    const pages = bookArr[2];
+    const read = bookArr[3];
+    const published = bookArr[4];
 
-let bookTwo = new Book("Frank Herbert", "Dune", 530, true, 1987,);
+    //element creation
+    const mainContainer = document.querySelector(".main-container");
+    const card =document.createElement('div');
+    card.classList.add("Book-Card");
+    card.style.display= 'grid';
+    card.textContent = `${bookArr} `;
 
-let bookThree = new Book("Danni Davido", "Magnum Dong", 2400, false, 2010,); 
+    //creating internal book-card content
+    //try foreach first
+    bookArr.map(book =>{
+        let bookItem =document.createElement('div');
+        bookItem.classList.add('author');
+        bookItem.innerHTML= `${book}`;
+        bookItem.style.backgroundColor = 'yellow';
+        card.appendChild(bookItem);
+    })
+    //author
+
+    /*
+    const bookAuthor = document.createElement('div');
+    bookAuthor.classList.add('author');
+    bookAuthor.innerHTML= `${author}`;
+    bookAuthor.style.backgroundColor = 'yellow';
+    card.appendChild(bookAuthor);
+    */
+
+
+    console.log(` this is the book card function , ${author}, ${title}, ${pages}, ${read}, ${published}, `)
+
+  
+  
+  
+    
+    card.style.backgroundColor = 'green';
+    //appending info into cards
+    mainContainer.appendChild(card);
+}
+
+// place holder example data
+let bookOne = new Book("Ekhart Tole","The Power Of Now", 300, false, 2001);
+let bookTwo = new Book("Frank Herbert", "Dune", 530, true, 1987);
+let bookThree = new Book("Danni Davido", "Magnum Dong", 2400, false, 2010); 
 
 myLibrary.push(bookOne);
 myLibrary.push(bookTwo);
 myLibrary.push(bookThree);
+
+
 displayBooks(myLibrary);
